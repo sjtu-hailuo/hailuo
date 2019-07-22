5da6b7081b3bc8f06e9838cfad55b69899fcb141
xlllq <liuxlllq@gmail.com>
2019-07-22 17:28:22 +0800

    update

---
+++ server/back_end/resources/static/scores/README.md
@@ -0,0 +1,3 @@
+# route on server
+/usr/local/musicxml/Beethoven_AnDieFerneGeliebte.xml
+/usr/local/musicxml/MuzioClementi_SonatinaOpus36No1_Part2.xml
---
+++ server/back_end/sql_init.sql
@@ -0,0 +1,42 @@
+drop table if exists user;
+create table user(
+    user_id int(11) NOT NULL auto_increment,
+    username varchar(20) NOT NULL,
+	password varchar(20) NOT NULL,
+    type enum('S','T','V') NOT NULL,
+    gender enum('M','F') NOT NULL,
+    primary key (user_id)
+);
+
+INSERT INTO user value(1,'hailuo','hailuo123','S','M');
+INSERT INTO user value(2,'sjtu','sjtu','T','M');
+select * from user;
+
+drop table if exists score;
+create table score(
+	id int(10) auto_increment not null,
+    title varchar(80) not null,
+    composer varchar(40) not null,
+    icon varchar(20) not null,
+    address varchar(100),
+    primary key(id)
+);
+
+insert into score values (1, 'Beethoven_AnDieFerneGeliebte','Beethoven','violin', '/usr/local/musicxml');
+insert into score values (2, 'MuzioClementi_SonatinaOpus36No1_Part2','Muzio Clementi','violin','/usr/local/musicxml');
+insert into score values (3, 'Saltarello_sample','none','violin','/usr/local/musicxml');
+select * from score;
+
+drop table if exists advice;
+create table advice(
+    id int(10) auto_increment not null,
+	user_id int(11) NOT NULL,
+	time TIMESTAMP NOT NULL,
+	ad varchar(200) NOT NULL,
+    primary key(id)
+);
+
+INSERT INTO advice value(1,3,'2019-7-15','haha');
+INSERT INTO advice value(2,5,'2019-7-14','soso');
+select * from advice;
+
